cf26aa071ca7df7545be0801fa173c735ddba75d
GSPaderz <bencapadbury@gmail.com>
2021-09-12 17:14:26 +0100

    Revert "I'm loosing my mind."
    This reverts commit 5f08b7ec64530be17016922dc8de14f5fdaa822f.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,3 @@
 import api_authorisation_token_scraper.apple.podcasts as scraper
 
-with open('authorisation_cache', 'w+') as file:
-    file.write(scraper.get_bearer_token())
+print(scraper.get_bearer_token())
