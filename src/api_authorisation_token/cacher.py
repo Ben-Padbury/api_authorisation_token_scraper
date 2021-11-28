f8bd464186b46ac06165621914b42ab4e28030fb
GSPaderz <bencapadbury@gmail.com>
2021-11-28 18:39:59 +0000

    Remove the cacher.

--- src/api_authorisation_token/cacher.py
+++
@@ -1,6 +0,0 @@
-filename = 'authorisation_cache'
-
-def cache(filename, value):
-    # Open the cache file, based on the given filename, and write the value to it.
-    with open(filename, 'w') as file:
-        file.write(value)
