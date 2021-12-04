8d1204d289acbf94c8512b4c8fc28ab981237a42
GSPaderz <bencapadbury@gmail.com>
2021-12-04 10:40:37 +0000

    It actually works now. That took a while. Damm apple and there reactivly loaded link events.

--- src/api_authorisation_token/api/apple/music.py
+++ src/api_authorisation_token/api/apple/music.py
@@ -11,19 +11,14 @@ class Music(Scraper):
     def __init__(self):
         super().__init__()
         self.api_scope = 'https://amp-api.music.apple.com/'
-        self.timeout = 300
-        self.url = 'https://music.apple.com/gb/album/whenever-you-need-somebody/1558533900'
+        self.timeout = 70
+        self.url = 'https://music.apple.com/gb/browse'
 
     def get_bearer_token(self, driver: Firefox) -> string:
-        # Wait for the body to gain the 'album' class, which to my best understanding indicates that the javascript
-        # application chunks have loaded, which should add the required event listener.
-        element = WebDriverWait(driver, self.timeout) \
-            .until(expected_conditions.visibility_of_element_located((By.CSS_SELECTOR, 'body')))
-        WebDriverWait(driver, self.timeout).until(lambda d: 'album' in element.get_attribute('class'))
-
         # Wait for the button which makes a request to the API to be visible, and then click it.
         WebDriverWait(driver, self.timeout) \
-            .until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, 'button.play-button')))
+            .until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, 'div[role=link]'))) \
+            .click()
 
         # Wait for a request which contains the authorisation token, and return it.
         return driver.wait_for_request(self.api_scope, self.timeout).headers.get('Authorization')
