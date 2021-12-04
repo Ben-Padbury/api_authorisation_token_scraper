aabeb25e9f4a3f6d8b48a209970e630c2172d83f
GSPaderz <bencapadbury@gmail.com>
2021-12-04 12:09:34 +0000

    Stop using a specific podcast. Using the generic page actually saves time, and automatically makes the API request for us.

--- src/api_authorisation_token/api/apple/podcasts.py
+++ src/api_authorisation_token/api/apple/podcasts.py
@@ -1,20 +1,8 @@
-import string
-
 from api_authorisation_token.scraper import Scraper
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions
-from selenium.webdriver.support.ui import WebDriverWait
-from seleniumwire.webdriver import Firefox
 
 
 class Podcasts(Scraper):
     def __init__(self):
         super().__init__()
         self.api_scope = 'https://amp-api.podcasts.apple.com/'
-        self.url = 'https://podcasts.apple.com/us/podcast/wrestling-with-johners-podcast/id1442108418'
-
-    def make_api_request(self, driver: Firefox) -> string:
-        # Wait for the button which makes a request to the API to be visible, and then click it.
-        WebDriverWait(driver, self.timeout) \
-            .until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, 'button.link:nth-child(1)'))) \
-            .click()
+        self.url = 'https://www.apple.com/apple-podcasts/'
